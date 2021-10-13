c72bf84f7e0cbd5daf2f424bb386a2f68f9c2a85
Tri Ngo <tringo@gmail.com>
2021-10-12 19:52:05 -0600

    think I finally got private route to work

--- src/components/privateroute.jsx
+++ src/components/privateroute.jsx
@@ -3,44 +3,54 @@
 
 // If they are: they proceed to the page
 // If not: they are redirected to the login page.
+import {url} from '../app';
 import { Redirect, Route } from 'react-router-dom'
 import React, {useState, useEffect} from 'react';
 
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-  const [isLoggedIn, setIsLoggedIn] = useState(null)
-  useEffect( () => {
-    const token = localStorage.getItem("token")
 
-    if(token){
-      fetch(`http://localhost:3000/is_token_valid`, {
+  const [auth, setAuth] = useState(false);
+  const [isTokenValidated, setIsTokenValidated] = useState(false);
+
+  useEffect(() => {
+    // send jwt to API to see if it's valid
+    let token = localStorage.getItem("token");
+    if (token) {
+      fetch(url+ '/is_token_valid', {
+        method: "GET",
         headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
           Authorization: `Bearer ${token}`
+        },
+      })
+      .then((res) => {
+        return res.json()
+      })
+      .then((json) => {
+        console.log(json)
+        if (json.status === 'True') {
+          setAuth(true);
         }
       })
-      .then(resp => resp.json())
-      .then(data => {
-          console.log(data)
-          setIsLoggedIn(true)})
-    }
-    else{
-      setIsLoggedIn(false)
+      .catch((err) => {
+        console.log(err)
+        setAuth(false);
+        localStorage.removeItem("token");
+      })
+      .then(() => setIsTokenValidated(true));
+    } else {
+       setIsTokenValidated(true); // in case there is no token
     }
 
   }, [])
+  if (!isTokenValidated) return <div />; // or some kind of loading animation
 
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isLoggedIn ? (
-          <Component {...props} />
-        ) : (
-          <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
-        )
-      }
-    />
-  )
+  return (<Route {...rest}
+    render={(props) => {
+      return auth ? <Component {...props} /> : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />
+    }} />)
 }
 
 export default PrivateRoute
