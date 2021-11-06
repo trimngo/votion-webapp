cd5242c4713f7602b453bf8836ad03855eecc4ee
Tri Ngo <tringo@gmail.com>
2021-11-06 14:33:58 -0600

    trying figure out why can't access environment variable

--- src/app.js
+++ src/app.js
@@ -16,8 +16,9 @@ import DisplayAvatar from './pages/displayavatar';
 import Proposals from './pages/proposals';
 import PrivateRoute from './components/privateroute';
 
+debugger
 const url = (process.env.NODE_ENV === 'production')?
-  process.env.API_URL:'http://localhost:3000/' 
+  process.env.URL:'http://localhost:3000/' 
 
 function NoMatch(){
   return(
