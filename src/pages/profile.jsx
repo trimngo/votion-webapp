234615f210c0a58368f1b60c1384999024dba18a
Tri Ngo <tringo@gmail.com>
2021-11-07 06:59:08 -0600

    save work before making changes

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -10,15 +10,52 @@ import {
 const apikey = process.env.REACT_APP_GIPHY_API_KEY
 const giphyFetch = new GiphyFetch(apikey);
 
-function Profile(){
+function ProfileImage(props){
     const width=193
     const height=130
-    const [userInfo, setUserInfo] = useState({user:{icon_url:""}})
     const [giphyData, setGiphyData] = useState(null)
-    const getUserData = async () => {
+    
+    const getGiphyData = async () => {
+        if(props.icon_url && !(props.icon_url?.toLowerCase().startsWith('http'))){
+            const gdata = await giphyFetch.gif(props.icon_url);
+            setGiphyData(gdata)
+            //console.log(JSON.stringify(gdata));
+        }
+
+    }
+    useEffect( () => {getGiphyData()} )
+
+    return(
+        <div>
+            {(giphyData)? 
+                <Gif gif={giphyData.data} width={300}/>:
+                <img src={props.icon_url}
+                        alt={props.icon_url} width={width} height={height} />
+            }
+        </div>
+    )
+}
+
+function RenderUser(props){
+    debugger
+    return(
+        <div>
+            <ProfileImage icon_url={props.userInfo.user.icon_url}/>
+            <pre>{JSON.stringify(props.userInfo, null, 2)} </pre>
+        </div>
+        
+    )
+}
+
+
+function Profile(){
+    const [dataStale, setDataStale] = useState(true)
+    const [usersInfo, setUsersInfo] = useState({users:[]})
+    
+    const getUsersData = async () => {
         const id = localStorage.getItem("id")
     
-        const resp = await fetch(api_url + 'users/' + id, {
+        const resp = await fetch(api_url + 'users', {
             method: "GET",
             headers: {
                 "Content-Type": "application/json",
@@ -27,92 +64,71 @@ function Profile(){
         });
         const data = await resp.json();
         console.log(data);
-        // data.user.icon_url="l41Yt8W2i7QxmGaic"
-        setUserInfo(data);
-
-        //get data for the user profile image
-        if(data.user.icon_url && !(data.user.icon_url?.toLowerCase().startsWith('http'))){
-            const gdata = await giphyFetch.gif(data.user.icon_url);
-            setGiphyData(gdata)
-            // console.log(JSON.stringify(gdata));
-        }
+        setUsersInfo(data);
+        setDataStale(false);
     }
-    
-    useEffect( () => {getUserData()} , [])
-
+        
+    useEffect( () => {getUsersData()} , [])
 
     var ReactS3Uploader = require('react-s3-uploader');
 
     var headers;
     var query_params;
+    // debugger
+    const renderUsers = usersInfo?.users.map((userInfo) =>
+        <RenderUser userInfo={userInfo} />
+    )
+
     return(
         <div>
-            {(giphyData)? 
-                <Gif gif={giphyData.data} width={300}/>:
-                <img src={userInfo.user.icon_url}
-                        alt={userInfo.user.icon_url} width={width} height={height} /> }
-            <pre>{JSON.stringify(userInfo, null, 2)} </pre>
-            
-        <ReactS3Uploader
-            signingUrl="/s3/sign"
-            signingUrlMethod="GET"
-            accept="image/*"
-            s3path="/uploads/"
-            // preprocess={(file, next) => {}}
-            // onSignedUrl={this.onSignedUrl}
-            // onProgress={this.onUploadProgress}
-            // onError={this.onUploadError}
-            onFinish={(signResult: { publicUrl: string; }) => {
-                //need upload to the server by modifying user info
-                const id = localStorage.getItem("id")
-                // user.name = params['user'].key?('name') ? params['user']['name'] : user.name
-                // user.icon_url = params['user'].key?('icon_url') ? params['user']['icon_url'] : user.icon_url
-                return fetch(api_url + 'users/' + id , {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Accept": "application/json"
-                    },
-                    body: JSON.stringify(
-                        {"user": {"icon_url":
-                            "https://votionapi.s3.us-west-1.amazonaws.com/"+signResult.publicUrl}}
-                    )
-                })
-                .then(resp => resp.json())
-                .then(data => {
-                    console.log(data)
-                    setUserInfo(data)
-                })
-                .then( data => {
+            {renderUsers}
+            <ReactS3Uploader
+                signingUrl="/s3/sign"
+                signingUrlMethod="GET"
+                accept="image/*"
+                s3path="/uploads/"
+                // preprocess={(file, next) => {}}
+                // onSignedUrl={this.onSignedUrl}
+                // onProgress={this.onUploadProgress}
+                // onError={this.onUploadError}
+                onFinish={(signResult: { publicUrl: string; }) => {
+                    //need upload to the server by modifying user info
+                    const id = localStorage.getItem("id")
+                    // user.name = params['user'].key?('name') ? params['user']['name'] : user.name
+                    // user.icon_url = params['user'].key?('icon_url') ? params['user']['icon_url'] : user.icon_url
                     return fetch(api_url + 'users/' + id , {
-                        method: "GET",
+                        method: "PUT",
                         headers: {
                             "Content-Type": "application/json",
                             "Accept": "application/json"
-                        }
+                        },
+                        body: JSON.stringify(
+                            {"user": {"icon_url":
+                                "https://votionapi.s3.us-west-1.amazonaws.com/"+signResult.publicUrl}}
+                        )
                     })
                     .then(resp => resp.json())
                     .then(data => {
                         console.log(data)
-                        setUserInfo(data)
+                        setDataStale(true)
                     })
-                })
-            }}
-            // signingUrlHeaders={{ additional: headers }}
-            // signingUrlQueryParams={{ additional: query_params }}
-            signingUrlWithCredentials={ false }      // in case when need to pass authentication credentials via CORS
-            uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }}  // this is the default
-            contentDisposition="auto"
-            scrubFilename={(filename: string) => {
-                let newfn=filename.replace(/[^\w\d_\-.]+/ig, '')
-                // debugger
-                // console.log(newfn)
-                return newfn
-            }}
-            server={api_url}
-            // inputRef={cmp => this.uploadInput = cmp}
-            autoUpload={true}
+                }}
+                // signingUrlHeaders={{ additional: headers }}
+                // signingUrlQueryParams={{ additional: query_params }}
+                signingUrlWithCredentials={ false }      // in case when need to pass authentication credentials via CORS
+                uploadRequestHeaders={{ 'x-amz-acl': 'public-read' }}  // this is the default
+                contentDisposition="auto"
+                scrubFilename={(filename: string) => {
+                    let newfn=filename.replace(/[^\w\d_\-.]+/ig, '')
+                    // debugger
+                    // console.log(newfn)
+                    return newfn
+                }}
+                server={api_url}
+                // inputRef={cmp => this.uploadInput = cmp}
+                autoUpload={true}
             />
+        <hr></hr>
         </div>
     )
 }
